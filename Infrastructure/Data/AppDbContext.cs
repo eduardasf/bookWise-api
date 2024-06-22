735e11a7023b52fcbdc26183f07fec25634d86db
Eduarda Souza <147343929+eduardasf@users.noreply.github.com>
2024-06-22 20:52:12 -0300

    base structure

---
+++ Infrastructure/Data/AppDbContext.cs
@@ -0,0 +1,32 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System.Xml;
+
+namespace Infrastructure.Data
+{
+     class AppDbContext : DbContext
+    {
+
+        public AppDbContext(DbContextOptions dbContextOptions)
+        : base(dbContextOptions)
+        {
+
+        }
+        public DbSet<Livro> Livros { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseNpgsql("Host=my_host;Database=my_db;Username=my_user;Password=my_pw");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            // Configurações adicionais do modelo
+        }
+    }
+}
